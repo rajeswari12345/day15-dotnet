652c3dd6bc5b94ee358ae81caf554a07ccf9ace7
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-13 14:09:51 +0530

    day15 uc1 binary

---
+++ day15 uc1 binary.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections;
+
+namespace BinarySearchTree
+{
+
+    public class BinarySearchTree
+    {
+
+        public class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
+            {
+                Console.Write(Data + " ");
+            }
+        }
+        public Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void Insert(int i)
+        {
+            Node newNode = new Node();
+            newNode.Data = i;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+
+                        else
+                        {
+                            current = current.Right;
+                            if (current == null)
+                            {
+                                parent.Right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            BinarySearchTree nums = new BinarySearchTree();
+            nums.Insert(56);
+            nums.Insert(30);
+            nums.Insert(70);
+            
+        }
+    }
+
+}
+
+
+ 
+
+
+
+
+
